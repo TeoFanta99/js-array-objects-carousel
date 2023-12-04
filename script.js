69d6625efed845b872ffb7b61a9d594e12986a81
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-04 15:35:52 +0100

    struttura html

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Dato un array di oggetti letterali con: - url dell’immagine - titolo - descriz
 Creare un carosello come nella foto allegata.
 
 Milestone 0:
-Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider.
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l’immagine grande in modo da poter stilare lo slider. FATTO
 
 Milestone 1:
 Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
@@ -13,33 +13,80 @@ Milestone 2:
 Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è la prima e l’utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l’ultima e viceversa per l’ultima miniatura se l’utente clicca la freccia verso sinistra.
 */
 
+// SELEZIONE degli elementi nel DOM
+const btnUp = document.querySelector(".fa-chevron-up");
+const btnDown = document.querySelector(".fa-chevron-down");
+const mainCarousel = document.getElementById("carosello")
+
+
+// FUNZIONE che permetta di generare elementi e di assegnare una classe
+function generaUnElemento (tagtype, classname) {
+    const element = document.createElement(tagtype);
+    element.classList.add(classname);
+    return element
+};
+
+
+// ARRAY di oggetti 
 const images = [ 
     
     { 
         image: 'img/01.webp', 
         title: 'Marvel\'s Spiderman Miles Morale', 
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.', 
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.'
     },  
     { 
         image: 'img/02.webp', 
         title: 'Ratchet & Clank: Rift Apart', 
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.', 
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.' 
     }, 
     { 
         image: 'img/03.webp', 
         title: 'Fortnite', 
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.", 
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos." 
     }, 
     { 
         image: 'img/04.webp', 
         title: 'Stray', 
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city', 
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city' 
     }, 
     { 
         image: 'img/05.webp', 
         title: "Marvel's Avengers", 
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.', 
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.' 
     } 
 
 ];
 console.log(images);
+
+
+
+// GENERARE la struttura HTML del carosello
+mainCarousel.innerHTML = `
+
+    <div class="slider">
+        <div class="items-container">
+            <i class="fa-solid fa-chevron-up"></i>
+            <i class="fa-solid fa-chevron-down"></i>
+        </div>
+        <img src="img/01.webp" alt="">
+    </div>
+
+`
+
+
+btnDown.addEventListener ("click", function () {
+
+    
+
+
+
+
+
+    }
+
+
+
+
+
+)
